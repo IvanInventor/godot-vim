c990c44aa19b36c39cc030cabdf88b19ddaf61fa
Vano <kavo@tavo.mail.com>
2023-03-25 13:54:31 +0200

    localize filepath for shorter tabnames

--- addons/godot-vim/godot-vim.sh
+++ addons/godot-vim/godot-vim.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 PWD=$1
-FILENAME=$2
+FILENAME=$(realpath --relative-to="$1" "$2") # TODO: filename without path as tab name
 LINE=$3
 COL=$4
 
 SOCKET_NAME="/tmp/godot-vim.$PPID"
+FOCUS_CMD='wmctrl -ia $WINDOWID'
 
 if [ -S "$SOCKET_NAME" ]; then
 	vim --server "$SOCKET_NAME" --remote-tab "$FILENAME"
 	vim --server "$SOCKET_NAME" --remote-expr "cursor($LINE,$COL)"
-	vim --server "$SOCKET_NAME" --remote-expr 'system("wmctrl -ia $WINDOWID")'
+	vim --server "$SOCKET_NAME" --remote-expr "system('$FOCUS_CMD')"
 else
 	alacritty -e vim --listen "$SOCKET_NAME" "+call cursor($LINE,$COL)" -- "$FILENAME"
 fi
